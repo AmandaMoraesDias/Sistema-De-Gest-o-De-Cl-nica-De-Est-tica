292d33c10a5a9ea73db40d1205da0a049c9a565f
Priscila Nicoli <priscilanicoli666@gmail.com>
2025-11-27 23:55:51 -0300

    Atualização do html e css

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,26 @@
-import fastify from "fastify";
+document.getElementById('formCliente').addEventListener('submit', function (e) {
+    e.preventDefault();
 
-const app = fastify()
+    const dados = {
+        nome: document.getElementById('nome').value,
+        email: document.getElementById('email').value,
+        data_nascimento: document.getElementById('data_nascimento').value,
+        observacoes: document.getElementById('observacoes').value
+    };
 
-app.listen({port:3001})
+    console.log('Enviando:', dados);
 
-
-
+    fetch('http://localhost:3000/clientes', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dados)
+    })
+    .then(res => res.json())
+    .then(resposta => {
+        console.log('Resposta backend:', resposta);
+        alert('Cliente salvo com sucesso!');
+    })
+    .catch(err => console.error(err));
+});
